971fc0de751c64967c5a57d2587eccf60f1185fe
jasenStudio <120119863+jasenStudio@users.noreply.github.com>
2024-04-07 19:03:29 -0500

    rest api movies

---
+++ app.js
@@ -0,0 +1,162 @@
+
+// * Instalaciones
+
+// * 1. npm i express -E
+// * 2. npm i zod     -E
+// * 3. npm i nodemon -D
+// * 4  npm i cors    -E
+
+
+
+//Importaciones y constantes
+const express = require('express'); 
+const app     = express();
+const crypto  = require('node:crypto');//Crear id dinamicos
+const cors    = require('cors');
+
+
+//* CONFIGURACIONES *//
+
+
+//Obtener el request en JSON
+app.use(express.json());
+
+//puerto
+const PORT = process.env.PORT ?? 1234;
+
+//Validaciones
+const { validateMovie,validatePartialMovie } = require('./schemas/movie');  
+
+//data
+const movies = require('./movies.json');
+
+
+//optionals
+app.disable('x-powered-by');
+
+
+//CORS
+
+
+
+app.use(cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        'http://localhost:8080',
+        'http://localhost:1234',
+        'https://movies.com',
+        'https://midu.dev'
+      ]
+  
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true)
+      }
+  
+      if (!origin) {
+        return callback(null, true)
+      }
+  
+      return callback(new Error('Not allowed by CORS'))
+    }
+  }))
+
+
+
+//* PETICIONES *//
+
+
+// *Todos la movies & generos
+app.get('/movies/', (req, res) => {
+
+    const { genre } = req.query;
+
+    if (genre) {
+        const filteredMovies = movies.filter(
+            movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+        )
+        return res.json(filteredMovies);
+    }
+
+    res.status(200).json(movies)
+})
+
+// * Single movie
+app.get('/movies/:id', (req, res) => {
+    const { id } = req.params;
+    const movie = movies.find(movie => movie.id === id);
+
+    if (movie) return res.json(movie)
+
+    res.status(404).json({ message: 'movie not found' })
+})
+
+// ! Create movie
+app.post('/movies', (req, res) => {
+
+    const result = validateMovie(req.body);
+
+    if (!result.success) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+
+    const newMovie = {
+        id: crypto.randomUUID(),
+        ...result.data
+    }
+
+
+
+    movies.push(newMovie);
+
+    res.status(201).json(newMovie)
+
+
+})
+
+// * Actualizar por segmento a un recurso 
+app.patch('/movies/:id',(req,res) => {
+    
+    const result     = validatePartialMovie(req.body);
+
+    const {id}       = req.params;
+    const movieIndex = movies.findIndex( m =>  m.id === id);
+
+
+    if (!result.success) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+
+    if(movieIndex === -1){
+        return res.status(404).json({message:'Movie not found'})
+    }
+
+    const updateMovie = {
+       ...movies[movieIndex],
+       ...result.data 
+    }
+
+    movies[movieIndex] = updateMovie;
+
+    return res.json(updateMovie);
+
+})
+
+app.delete('/movies/:id', (req, res) => {
+    const { id } = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+  
+    if (movieIndex === -1) {
+      return res.status(404).json({ message: 'Movie not found' })
+    }
+  
+    movies.splice(movieIndex, 1)
+  
+    return res.json({ message: 'Movie deleted' })
+  })
+
+
+app.listen(PORT, () => {
+    console.log(`server listening on port http://localhost:${PORT}`)
+})
